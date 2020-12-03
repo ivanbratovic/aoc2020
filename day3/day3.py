2a5c4cca23a1d25d8e7af351a5c32dc49220b2fd
Ivan Bratović <ivan.bratovic@fer.hr>
2020-12-03 08:24:25 +0100

    day3: add solver and input file
    Signed-off-by: Ivan Bratović <ivan.bratovic@fer.hr>

---
+++ day3/day3.py
@@ -0,0 +1,56 @@
+import numpy as np
+import sys
+import re
+
+def map_from_file(filepath : str):
+    """
+    Reads input and returns a numpy.ndarray
+    representing the main map data.
+    """
+    list_lines = []
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            l = []
+            for char in line.strip():
+                l.append(char)
+            list_lines.append(l)
+    return np.array(list_lines)
+
+def map_move(coords : np.array, map : np.ndarray, move : np.array):
+    """
+    Moves the coordinates according to the move,
+    and returns the location (. or #) of the new
+    coordinates using the map.
+    """
+    coords += move
+    coords[1] = coords[1] % map.shape[1]
+    if (coords[0] >= (map.shape[0])):
+        return coords, None
+    return coords, map[tuple(coords.tolist())]
+
+def main():
+    """
+    Main function. Contains primary logic.
+    """
+    area_map =  map_from_file("input.txt")
+    moves = list(map(np.array, [[1,1],[1,3],[1,5],[1,7],[2,1]]))
+    
+    product = 1
+    for move in moves:
+        location = area_map[0,0]
+        coords = np.array([0,0])
+        trees = 0
+
+        while location is not None:
+            # Execute moves until you can't anymore
+            if location == "#":
+                trees += 1
+            coords, location = map_move(coords, area_map, move)
+
+        print(trees)
+        product *= trees
+        
+    print("\n"+str(product))
+
+main()
