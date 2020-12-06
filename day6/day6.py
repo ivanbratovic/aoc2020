b7e07750fb187e9f465b02c70c5190841f7728d6
Ivan Bratović <ivan.bratovic@fer.hr>
2020-12-06 06:32:30 +0100

    day6: add all files, add stars to README
    Signed-off-by: Ivan Bratović <ivan.bratovic@fer.hr>

---
+++ day6/day6.py
@@ -0,0 +1,57 @@
+import numpy as np
+import sys
+import re
+
+def voting_groups_from_file(filepath : str):
+    """
+    Reads input from file and returns two things:
+     + Sets of group votes (with unique votes for the group) for part 1
+     + All group votes by person for part 2
+    """
+    groups = []
+    group_sets = []
+    with open(filepath, "r") as file:
+        group_set = set()
+        answers = []
+        for line in file.readlines():
+            person = []
+            if line.strip() == "":
+                groups.append(answers)
+                group_sets.append(group_set)
+                group_set = set()
+                answers = []
+                continue
+            for i, char in enumerate(line.strip()):
+                person.append(char)
+                group_set.add(char)
+            answers.append(person)
+        groups.append(answers)
+        group_sets.append(group_set)
+    return groups, group_sets
+
+def main():
+    """
+    Main function. Contains primary logic.
+    """
+    groups, group_sets =  voting_groups_from_file("input.txt")
+    # Part 1
+    s = 0
+    for group in group_sets:
+        s += len(group)
+    print(s)
+    # Part 2
+    s = 0
+    for group in groups:
+        count = len(group)
+        d = {}
+        for person in group:
+            for vote in person:
+                d.setdefault(vote, 0)
+                d[vote] += 1
+        for k in d.keys():
+            if d[k] == count:
+                s+=1
+    print(s)
+    # Part 2
+
+main()
